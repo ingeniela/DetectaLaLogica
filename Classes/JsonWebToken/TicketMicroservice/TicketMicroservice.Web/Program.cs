d566d26d4f4bc2f4dd8476680df2de3514fd5ca8
Daniela Barazarte <129801345+danielabarazarteps@users.noreply.github.com>
2023-05-26 18:18:42 -0400

    Update Program.cs

--- Classes/JsonWebToken/TicketMicroservice/TicketMicroservice.Web/Program.cs
+++ Classes/JsonWebToken/TicketMicroservice/TicketMicroservice.Web/Program.cs
@@ -97,7 +97,6 @@ app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.RoutePrefix = "tickets";
 
     // Add the authorization token input box to Swagger UI
     options.OAuthClientId("swagger");
