5b0c3b449655ed342eea477bed9b7c830da8eee5
Daniela Barazarte <129801345+danielabarazarteps@users.noreply.github.com>
2023-06-02 18:08:41 -0400

    Concepto nuevo! interfaz

---
+++ Conceptos/Interfaz/DetectaLaLogica.CSharp.Interfaz/DetectaLaLogica.CSharp.Interfaz/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public interface IAnimal
+{
+    void HacerSonido();
+    void LlamarAmigo();
+}
+
+public class Perro : IAnimal
+{
+    public void HacerSonido()
+    {
+        Console.WriteLine("¡Guau! Woof!");
+    }
+    public void LlamarAmigo()
+    {
+        Console.WriteLine("¡Ven amigo perro!");
+    }
+}
+
+public class Gato : IAnimal
+{
+    public void HacerSonido()
+    {
+        Console.WriteLine("¡Miau! Meow!");
+    }
+    public void LlamarAmigo()
+    {
+        Console.WriteLine("¡Ven amigo gato!");
+    }
+}
+public class Pajaro : IAnimal
+{
+    public void HacerSonido()
+    {
+        Console.WriteLine("¡Pío Pío! Quququ!");
+    }
+
+    public void LlamarAmigo()
+    {
+        Console.WriteLine("¡Ven amigo pájaro!");
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        IAnimal miGato = new Gato();
+        IAnimal miPerro = new Perro();
+        IAnimal miPajaro = new Pajaro();
+
+        miGato.HacerSonido();
+        miGato.LlamarAmigo();
+
+        miPerro.HacerSonido();
+        miPerro.LlamarAmigo();
+
+        miPajaro.HacerSonido();
+        miPajaro.LlamarAmigo();
+    }
+}
